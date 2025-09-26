cda42dbc63228f1a5d797dfa6c10bb0e08f9c49c
PrinceRohithA <princerohith.a.2024.cse@rajalakshmi.edu.in>
2025-09-26 23:40:55 +0530

    two level completed and little progress

--- rocket-dash/ui/play_again.gd
+++ rocket-dash/ui/play_again.gd
@@ -5,6 +5,9 @@ extends CanvasLayer
 
 var ps: PackedScene = load("res://rocket-dash/game.tscn") 
 
+func _ready() -> void:
+	label.text = "Your Score: " + str(Global.score)
+
 func _on_button_pressed() -> void:
 	Global.score = 0
 	get_tree().change_scene_to_packed(ps)
